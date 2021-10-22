ff1e5597ad96ba2fa20a0a9fb0a1e930e37c769e
月成 <694220741@qq.com>
2021-10-22 11:20:41 +0800

    fix:卡顿

--- Pica/Chat/Controller/PCChatViewController.m
+++ Pica/Chat/Controller/PCChatViewController.m
@@ -67,7 +67,9 @@ static CGFloat const kChatBarTextViewMaxHeight = 102.f;
     [super viewDidLoad];
  
     [self connect];
-    [self localMessageArray];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self localMessageArray];
+    });
 }
 
 - (void)initSubviews {
