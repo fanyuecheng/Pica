3c92006817cfb4cf8d98b0cd11c0df1191abe7b5
月成 <694220741@qq.com>
2022-08-15 16:08:55 +0800

    fix:三方更新

--- Pods/SDWebImage/SDWebImage/Core/SDWebImageManager.m
+++ Pods/SDWebImage/SDWebImage/Core/SDWebImageManager.m
@@ -382,6 +382,11 @@ static id<SDImageLoader> _defaultImageLoader;
                               cacheType:(SDImageCacheType)cacheType
                                progress:(nullable SDImageLoaderProgressBlock)progressBlock
                               completed:(nullable SDInternalCompletionBlock)completedBlock {
+    // Mark the cache operation end
+    @synchronized (operation) {
+        operation.cacheOperation = nil;
+    }
+    
     // Grab the image loader to use
     id<SDImageLoader> imageLoader;
     if ([context[SDWebImageContextImageLoader] conformsToProtocol:@protocol(SDImageLoader)]) {
@@ -552,11 +557,6 @@ static id<SDImageLoader> _defaultImageLoader;
                                cacheType:(SDImageCacheType)cacheType
                                 finished:(BOOL)finished
                                completed:(nullable SDInternalCompletionBlock)completedBlock {
-    // the target image store cache type
-    SDImageCacheType storeCacheType = SDImageCacheTypeAll;
-    if (context[SDWebImageContextStoreCacheType]) {
-        storeCacheType = [context[SDWebImageContextStoreCacheType] integerValue];
-    }
     id<SDImageTransformer> transformer = context[SDWebImageContextImageTransformer];
     if (![transformer conformsToProtocol:@protocol(SDImageTransformer)]) {
         transformer = nil;
@@ -661,7 +661,7 @@ static id<SDImageLoader> _defaultImageLoader;
     BOOL shouldThumbnailImage = context[SDWebImageContextImageThumbnailPixelSize] != nil || image.sd_decodeOptions[SDImageCoderDecodeThumbnailPixelSize] != nil;
     
     // Store the transformed/thumbnail image into the cache
-    if (transformed || shouldThumbnailImage) {
+    if (image && (transformed || shouldThumbnailImage)) {
         NSData *cacheData;
         // pass nil if the image was transformed/thumbnailed, so we can recalculate the data from the image
         if (cacheSerializer && (storeCacheType == SDImageCacheTypeDisk || storeCacheType == SDImageCacheTypeAll)) {
